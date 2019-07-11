bbcbf337e86e3b00880e294b9675e12f6f3a5e79
muskan <47139210+muskangupta31@users.noreply.github.com>
2019-07-11 13:10:06 +0530

    Rest api

---
+++ src/main/java/Customer.java
@@ -0,0 +1,28 @@
+package net.viralpatel.spring.model;
+
+import java.util.Date;
+
+public class Customer {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String mobile;
+	private Date dateOfBirth;
+
+	public Customer(long id, String firstName, String lastName, String email, String mobile) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.mobile = mobile;
+		this.dateOfBirth = new Date();
+	}
+
+	public Customer() {
+	}
+
+	//..Getter and setter methods
+
+}
